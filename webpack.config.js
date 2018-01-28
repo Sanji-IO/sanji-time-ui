31bbe9b08dfe5160ad9fc4216f2827c9c9d98d10
Zack Yang <zack9433@gmail.com>
2018-01-28 16:46:18 +0800

    fix(package): modify to peer dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ const config = {
       'angular-material-icons.css': nodeRoot + '/angular-material-icons/angular-material-icons.css',
       'angular-material-data-table.css': nodeRoot + '/angular-material-data-table/dist/md-data-table.css',
       'angular-sanji-window.css': nodeRoot + '/angular-sanji-window/dist/angular-sanji-window.css',
+      'easy-pie-chart': nodeRoot + '/easy-pie-chart/dist/angular.easypiechart.js',
       'toastr.css': nodeRoot + '/toastr/build/toastr.css'
     },
     extensions: ['.js', '.json', 'html', 'scss', 'css']
@@ -26,7 +27,6 @@ const config = {
     rules: [
       { test: /\.js$/, use: 'eslint-loader', exclude: /node_modules/, enforce: 'pre' },
       { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.json$/, use: 'json-loader', exclude: /node_modules/ },
       {
         test: /\.html$/,
         use: 'ng-cache-loader?prefix=[dir]/[dir]',
